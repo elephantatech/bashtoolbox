4525952ff422b0a5ef324d8f2dbc6c57f2f3db44
Elephantatech <vivekm.mistry@gmail.com>
2017-03-19 03:19:02 -0400

    updated diagnostics script

--- diagnostics/simple_diagnostics.sh
+++ diagnostics/simple_diagnostics.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+# author: elephantatech
+# date: 2017-03-19
+# script to get some information from the linux system
+#
 PRODUCT=`cat /etc/issue`
 CURRENTDATE=`date +%Y%m%d_%H%M%S`
 HOST=`hostname -s`
