5629b9b32bd69259e9c0a549c54fc18222a9d52c
elephantatech <vivekm.mistry@gmail.com>
2017-03-19 00:59:25 -0400

    updated

--- diagnostics/simple_diagnostics.sh
+++ diagnostics/simple_diagnostics.sh
@@ -2,4 +2,4 @@ PRODUCT=`cat /etc/issue`
 CURRENTDATE=`date +%Y%m%d_%H%M%S`
 HOST=`hostname -s`
 TMPLOGDIR=/tmp/diagnostics
-ARCHIVE=/tmp/diagnostics.${HOST}.${CURRENTDATE}
+ARCHIVE=/tmp/diagnostics.${HOST}.${CURRENTDATE}.tar.gz
